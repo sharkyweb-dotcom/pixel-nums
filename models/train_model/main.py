150b8b5aa711303b9a61b5f2e64e0439c9e37e98
Richard Archer <richard.archer@yale.edu>
2023-08-14 09:59:07 -0400

    lets go

---
+++ models/train_model/main.py
@@ -0,0 +1,72 @@
+import torch
+import torch.nn as nn
+import torch.optim as optim
+import torchvision.transforms as transforms
+from torchvision.datasets import MNIST
+from torch.utils.data import DataLoader
+
+# Download and load MNIST dataset
+transform = transforms.Compose([transforms.ToTensor(), transforms.Normalize((0.5,), (0.5,))])
+
+train_dataset = MNIST(root='./data', train=True, transform=transform, download=True)
+test_dataset = MNIST(root='./data', train=False, transform=transform)
+
+train_loader = DataLoader(train_dataset, batch_size=64, shuffle=True)
+test_loader = DataLoader(test_dataset, batch_size=64, shuffle=False)
+
+# Define the CNN model
+class SimpleCNN(nn.Module):
+    def __init__(self):
+        super(SimpleCNN, self).__init__()
+        self.conv1 = nn.Conv2d(1, 32, kernel_size=3, stride=1, padding=1)
+        self.conv2 = nn.Conv2d(32, 64, kernel_size=3, stride=1, padding=1)
+        self.fc1 = nn.Linear(64*28*28, 128)
+        self.fc2 = nn.Linear(128, 10)
+
+    def forward(self, x):
+        x = torch.relu(self.conv1(x))
+        x = torch.relu(self.conv2(x))
+        x = x.view(x.size(0), -1)
+        x = torch.relu(self.fc1(x))
+        x = self.fc2(x)
+        return x
+
+model = SimpleCNN()
+criterion = nn.CrossEntropyLoss()
+optimizer = optim.Adam(model.parameters(), lr=0.001)
+
+# Training the model
+num_epochs = 5
+for epoch in range(num_epochs):
+    model.train()
+    train_loss = 0.0
+    correct_train = 0
+
+    for inputs, labels in train_loader:
+        optimizer.zero_grad()
+        outputs = model(inputs)
+        loss = criterion(outputs, labels)
+        loss.backward()
+        optimizer.step()
+        train_loss += loss.item()
+        _, predicted = outputs.max(1)
+        correct_train += predicted.eq(labels).sum().item()
+
+    train_accuracy = 100. * correct_train / len(train_dataset)
+
+    model.eval()
+    correct_test = 0
+    with torch.no_grad():
+        for inputs, labels in test_loader:
+            outputs = model(inputs)
+            _, predicted = outputs.max(1)
+            correct_test += predicted.eq(labels).sum().item()
+
+    test_accuracy = 100. * correct_test / len(test_dataset)
+
+    print(f"Epoch {epoch+1}/{num_epochs}")
+    print(f"Training Loss: {train_loss/len(train_loader):.4f}, Training Accuracy: {train_accuracy:.2f}%")
+    print(f"Testing Accuracy: {test_accuracy:.2f}%")
+    print("-----------------------------")
+
+torch.save(model.state_dict(), 'mnist_cnn_weights.pth')
