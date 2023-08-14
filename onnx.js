42cdc401f2504cedbc189edb9065a5795b6aa5b5
HappyCoder374 <finnharcher@gmail.com>
2023-08-14 12:28:32 -0500

    2023.08.14.12.28

--- onnx.js
+++ onnx.js
@@ -29,14 +29,15 @@ async function updatePredictions(imgData) {
   // Get the predictions for the canvas data.
   // const input = new onnx.Tensor(imgData, 'float32',[1,1,28,28]);
 
-  const flatData = new Float32Array(1 * 1 * 28 * 28).fill(0);
-  const input = new onnx.Tensor(flatData, 'float32', [1, 1, 28, 28]);
+  // const flatData = new Float32Array(1 * 1 * 28 * 28).fill(0);
+  // const input = new onnx.Tensor(flatData, 'float32', [1, 1, 28, 28]);
 
   const outputMap = await sess.run([input]);
   const outputTensor = outputMap.values().next().value;
   const predictions = outputTensor.data;
   const maxPrediction = Math.max(...predictions);
   console.log(maxPrediction)
+  alert(predictions.indexOf(maxPrediction))
   /*for (let i = 0; i < predictions.length; i++) {
     const element = document.getElementById(`prediction-${i}`);
     element.children[0].children[0].style.height = `${predictions[i] * 100}%`;
